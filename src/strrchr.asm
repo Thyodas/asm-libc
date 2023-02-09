59d752763d3942a71e8f1bc616e5d1e402d5e173
Thyodas <guillaume.hein@epitech.eu>
2023-02-09 17:34:29 +0100

    feat: improve strrchr

--- src/strrchr.asm
+++ src/strrchr.asm
@@ -7,25 +7,20 @@ strrchr:
     push    rbp
     mov     rbp, rsp
 
-    mov rax, rdi
-    cmp byte [rdi], sil
-    je end
     xor rax, rax
-    cmp byte [rdi], 0
-    je end
     loop:
-        inc rdi
-
         cmp byte [rdi], sil
         je  found
 
         cmp byte [rdi], 0
         jz  end
 
+        inc rdi
         jmp loop
         found:
             mov rax, rdi
-            cmp byte [rdi], 0
+            inc rdi
+            cmp byte [rax], 0
             jnz loop
     end:
 
